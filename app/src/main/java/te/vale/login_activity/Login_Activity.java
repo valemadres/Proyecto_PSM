8783e81824f294a55bae7bab0ed9b0cca50b9557
Noel Vallejos Ugarte <noel.72712510.va@gmail.com>
2022-10-05 09:18:27 -0400

    Configurando nuestro tema MaterialURL

---
+++ app/src/main/java/te/vale/login_activity/Login_Activity.java
@@ -0,0 +1,14 @@
+package te.vale.login_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
